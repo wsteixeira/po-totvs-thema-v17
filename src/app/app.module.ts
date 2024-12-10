ac790291b5e79f248d3943b51126a154f147fb93
Wanderley Teixeira <wanderley.teixeira@totvs.com.br>
2024-12-10 12:03:42 -0300

    Adiciona PO-UI e Totvs-tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
